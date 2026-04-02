6873cccfb30b59eb93126145f74f71e6002d18e7
Rio Bérénos <r.berenos@hotmail.com>
2026-04-02 14:50:45 +0200

    Update

--- aframe/index.js
+++ aframe/index.js
@@ -82,7 +82,7 @@ export function SuspiciousIsland() {
 								<!-- Light Source -->
 								<a-entity
 									id="playerLight"
-									light="type: spot; color: #ffffff; intensity: 2.0; angle: 45; distance: 60; decay: 1.5; penumbra: 0.1;"
+									light="type: spot; color: #ffffff; intensity: 2.0; angle: 50; distance: 60; decay: 1.5; penumbra: 0.1;"
 									position="0 0 -0.2"
 									light-jitter>
 								</a-entity>
@@ -133,8 +133,48 @@ export function SuspiciousIsland() {
 
 
 AFRAME.registerComponent("light-jitter", {
+	schema: {
+		rotAmount: { default: 0.001 },
+		flickerSpeed: { default: 500000 },
+		intensityVar: { default: 0.25 },
+		angleVar: { default: 0.05 },
+		distanceVar: { default: 0.25 }
+	},
+
+	init: function () {
+		this.baseRotation = this.el.object3D.rotation.clone();
+
+		const light = this.el.getAttribute("light");
+		this.baseIntensity = light.intensity || 3;
+		this.baseAngle = light.angle || 20;
+		this.baseDistance = light.distance || 20;
+	},
+
 	tick: function (time) {
-		this.el.object3D.rotation.x += (Math.random() - 0.5) * 0.005;
-		this.el.object3D.rotation.y += (Math.random() - 0.5) * 0.005;
+		const obj = this.el.object3D;
+
+		obj.rotation.x = this.baseRotation.x + Math.sin(time / 300) * this.data.rotAmount;
+		obj.rotation.y = this.baseRotation.y + Math.cos(time / 400) * this.data.rotAmount;
+
+		const flicker = Math.sin(time / this.data.flickerSpeed);
+
+		const intensity =
+			this.baseIntensity +
+			flicker * this.data.intensityVar +
+			(Math.random() - 0.5) * 0.2;
+
+		const angle =
+			this.baseAngle +
+			Math.sin(time / 500) * this.data.angleVar;
+
+		const distance =
+			this.baseDistance +
+			Math.cos(time / 600) * this.data.distanceVar;
+
+		this.el.setAttribute("light", {
+			intensity: intensity,
+			angle: angle,
+			distance: distance
+		});
 	}
 });
